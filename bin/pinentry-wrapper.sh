9acc5e85dca9621c8cb426bf74435340b8149b16
Andrew Sveikauskas <a.l.sveikauskas@gmail.com>
2021-02-02 15:44:57 -0800

    pinentry-wrapper: add `grep -v ^no` to which call.
    I had some old unix box where `which` would show a message like "no $i in ..."
    for the not found case.

--- bin/pinentry-wrapper.sh
+++ bin/pinentry-wrapper.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 exec `(which pinentry-gtk-2; 
        ls /Applications/MacPorts/pinentry-mac.app;
-       which pinentry) 2>/dev/null | head -n 1` "$@"
+       which pinentry) 2>/dev/null | grep -v ^no | head -n 1` "$@"
